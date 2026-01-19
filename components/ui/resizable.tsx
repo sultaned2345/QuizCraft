70eb70dd5d0f5b600c4c106967878316ac792a6e
sultaned2345 <sultanalshebi569@gmail.com>
2026-01-19 08:28:47 +0000

    987ytgyui

--- components/ui/resizable.tsx
+++ components/ui/resizable.tsx
@@ -1,10 +1,9 @@
-// components/ui/resizable.tsx
 "use client"
 
 import * as React from "react"
 import { GripVertical } from "lucide-react"
 
-import { cn } from "@/lib/utils" // This import assumes @/lib/utils.ts, which should resolve via src/lib/utils.ts
+import { cn } from "@/lib/utils"
 import {
   PanelGroup as PanelGroupPrimitive,
   Panel as PanelPrimitive,
@@ -35,7 +34,7 @@ const ResizableHandle = React.forwardRef<
   }
 >(({ className, withHandle, ...props }, ref) => (
   <PanelResizeHandlePrimitive
-    ref={ref}
+    ref={ref as any} // FIX: Cast ref to any to bypass strict type check
     className={cn(
       "relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0 [&[data-panel-group-direction=vertical]>div]:rotate-90",
       className
