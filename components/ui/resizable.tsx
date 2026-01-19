7c2486cf05ef6cc84d89fde46b158f50795e14c5
sultaned2345 <sultanalshebi569@gmail.com>
2026-01-19 08:32:16 +0000

    23we

--- components/ui/resizable.tsx
+++ components/ui/resizable.tsx
@@ -1,3 +1,4 @@
+// components/ui/resizable.tsx
 "use client"
 
 import * as React from "react"
@@ -34,7 +35,8 @@ const ResizableHandle = React.forwardRef<
   }
 >(({ className, withHandle, ...props }, ref) => (
   <PanelResizeHandlePrimitive
-    ref={ref as any} // FIX: Cast ref to any to bypass strict type check
+    // @ts-ignore: Library type definition is missing ref prop, but it is supported at runtime.
+    ref={ref}
     className={cn(
       "relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0 [&[data-panel-group-direction=vertical]>div]:rotate-90",
       className
